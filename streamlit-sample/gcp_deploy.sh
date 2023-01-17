2f74d9577658c08b9241cfff29bd32e912fad295
Hirotaka Owada <46743165+HOowada@users.noreply.github.com>
2023-01-17 13:51:25 +0900

    Delete streamlit-sample directory

--- streamlit-sample/gcp_deploy.sh
+++
@@ -1,6 +0,0 @@
-project=$1
-# image build
-gcloud builds submit --tag asia.gcr.io/$project/streamlit-baseball
-
-# deploy
-gcloud run deploy --image asia.gcr.io/${project}/streamlit-baseball --platform managed --memory 512M
